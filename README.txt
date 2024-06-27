874bce5befd0eec1146ba3c2d6cff27be96194e4
OttoBecker <ottoadrianobecker@gmail.com>
2024-06-26 22:25:38 -0300

    Criação automática das tabelas necessárias + README

---
+++ README.txt
@@ -0,0 +1,13 @@
+Passo a passo para a exeução do sistema
+
+1 - Instale o NodeJs;
+
+2 - Clone o repositório;
+
+3 - Ajuste os dados do banco de dados, como o user, password e database, no arquivo db.js dentro da pasta src;
+
+4 - Utilize o comando "npm install" no terminal para instalar as dependências necessárias;
+
+5 - Utilize o comando "npm init -y no terminal" caso não possua o arquivo "package-lock.json";
+
+6 - Utilize o comando "npm run iniciarProjeto" para inicar o projeto;
--- src/db.js
+++ src/db.js
@@ -4,15 +4,50 @@ const con = mysql.createConnection({
     host: 'localhost', //Host
     user: 'otto_becker', //Usuário
     password: 'teste123', //Senha
-    database: 'forum_tematico' //Banco
+    database: 'forum_tematico', //Banco
 });
 
 con.connect((erro) => {
     if (erro) {
-        console.error('Erro ao conectar ao MySQL:', erro);
-        throw erro
+        console.error('Erro ao conectar ao MySQL');
+        throw erro;
     }
     console.log('Conectado ao MySQL com sucesso!')
+    let sql = ` CREATE TABLE IF NOT EXISTS CLIENTES 
+                (id_cliente int primary key auto_increment not null,
+                nome varchar(255) not null,
+                cpf varchar(40) not null,
+                dataCadastro datetime not null);`;
+    con.execute(sql, (erro, retorno) =>{
+        if(erro){
+            console.error('Erro ao criar as tabelas necessárias:', erro)
+            throw erro
+        } else if (retorno.warningStatus == 0){
+            console.log('Criado tabela de CLIENTES')
+        }
+        
+    });
+
+    sql = `     CREATE TABLE IF NOT EXISTS COMPRAS
+                (id_compra int primary key auto_increment not null,
+                descricao varchar(255) not null,
+                valor float not null,
+                metodoPagamento VARCHAR(45),
+                dataCompra DATETIME,
+                id_cliente int not null,
+                CONSTRAINT FK_COMPRAS FOREIGN KEY (id_cliente) REFERENCES CLIENTES(id_cliente) ON UPDATE CASCADE ON DELETE CASCADE)`;
+
+    con.execute(sql, (erro, retorno) =>{
+        if(erro){
+            console.error('Erro ao criar as tabelas necessárias:', erro)
+            throw erro
+        } else if (retorno.warningStatus == 0){
+            console.log('Criado tabela de COMPRAS')
+        }
+        
+    });
+
+
 })
 
 module.exports = con
